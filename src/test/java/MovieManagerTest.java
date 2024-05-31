a8f6677d57c6b4e6c6ee14f0025efab2719e12d2
VikaEra <vikastav26@yandex.ru>
2024-05-31 13:18:38 +0300

    failes added

---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+    @Test
+    public void test() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film I", "Film II", "Film III"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
+
+
+
+
