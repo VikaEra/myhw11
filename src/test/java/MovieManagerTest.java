4cb8a775a69c951a817a6b245cc68c9fe44138b8
VikaEra <vikastav26@yandex.ru>
2024-06-01 14:22:23 +0300

    failess added

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,10 +1,12 @@
+package org.example;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MovieManagerTest {
 
     @Test
-    public void test() {
+    public void findAll() {
         MovieManager manager = new MovieManager();
         manager.addMovie("Film I");
         manager.addMovie("Film II");
@@ -12,6 +14,18 @@ public class MovieManagerTest {
 
         String[] expected = {"Film I", "Film II", "Film III"};
         String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
 }
